14dcd88a7ad50f304dd8546d9b0ccac4283b0641
Takashi Yoshimi <tayoshimi@gmail.com>
2024-12-14 10:06:27 +0900

    add csv read sample

--- example/csv_read_sjis/src/main.rs
+++ example/csv_read_sjis/src/main.rs
@@ -2,13 +2,15 @@ extern crate csv;
 
 use std::env;
 use std::error::Error;
-use std::fs::File;
+use std::fs;
 use std::io::Read;
 use std::process;
+use encoding_rs;
 
-fn fopen(path: &str) -> Result<File, Box<dyn Error>> {
-    let file = File::open(path)?;
-    Ok(file)
+fn fopen_sjis(filename: &str) -> Result<String, Box<dyn Error>> {
+    let buf = fs::read(filename)?;
+    let (dec, _, _) = encoding_rs::SHIFT_JIS.decode(&buf);
+    return Ok(dec.into_owned());
 }
 
 fn csv_parse<R: Read>(read: R) -> Result<(), Box<dyn Error>> {
@@ -26,7 +28,7 @@ fn csv_parse<R: Read>(read: R) -> Result<(), Box<dyn Error>> {
 fn main() {
     let file_path = "./res/sample.csv";
     
-    match csv_parse(fopen(&file_path).unwrap()) {
+    match csv_parse(fopen_sjis(&file_path).unwrap().as_bytes()) {
         Ok(_) => println!("Successfully parsed CSV"),
         Err(err) => {
             eprintln!("Error: {}", err);
